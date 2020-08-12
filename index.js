0085f4431389daac26fde4277a6f8e13d55fb336
Ahmetcan Aksu <ahmetcanco@gmail.com>
2020-08-13 02:18:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Gpio = require('onoff').Gpio;
+
 module.exports = function (fanPin, {min, max}) {
 	var degree = () => new Promise((resolve) => {
 		require('child_process').exec("/opt/vc/bin/vcgencmd measure_temp").stdout.on('data', (data) => {
@@ -9,7 +10,7 @@ module.exports = function (fanPin, {min, max}) {
 	if(fanPin instanceof Gpio) {
 		var {maxT, minT} = {min: min && !isNaN(Number(min)) || 45, maxT: max && !isNaN(Number(max)) || 55}
 		setInterval(async () => {
-			var cpuDegree = await degrec.get();
+			var res = await degrec.get();
 			res > maxT || res == maxT ? fanPin.writeSync(1) : fanPin.writeSync(0) 
 		}, 100)
 	} else {
