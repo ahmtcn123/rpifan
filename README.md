c8e9a1e673ef620e85177b409a70a981ece6fef4
Ahmetcan Aksu <ahmetcanco@gmail.com>
2020-08-13 02:17:34 +0300

    Rebuild for some reason

--- README.md
+++ README.md
@@ -1,23 +1,35 @@
 # rpifan
 Auto fan controller for rpi
 
-_____________________________
+### NPM
 ```shell
-
 $ npm i rpifan
-
 ```
-_____________________________
 
-
-Its basic as learning name!
+### Require Module
 
 ```js
-var fan = require("rpifan")
-new fan(17,{"min":47,"max":52})
-
-//Or left it blank to select auto mod
-//new fan(17)
+    var fan = require("rpifan")
 ```
 
+
+### Set max & min
+
+```js
+    fan(new Gpio(11, "out"), {
+        min:47,
+        max:52
+    })
+```
+
+### Auto mode
+
+```js
+    fan(new Gpio(11, "out"))
+```
+
+
 <img src="https://image.ibb.co/ijT4GV/fan.png" height="350">
+
+
+`You can see things I learn in 1 year if you look commits`
--- index.js
+++ index.js
@@ -1,50 +1,18 @@
 const Gpio = require('onoff').Gpio;
-const degrec = require("./degree.js")
-const ps = require("os")
+module.exports = function (fanPin, {min, max}) {
+	var degree = () => new Promise((resolve) => {
+		require('child_process').exec("/opt/vc/bin/vcgencmd measure_temp").stdout.on('data', (data) => {
+		  resolve(data.toString().split("=")[1].split(".")[0])
+		});
+	})
 
-module.exports = function (gpio,val) {
-	this.val  = val
-	this.gpio = gpio
-if(ps.type() !== "Linux") {
-throw new Error("This os not supported: " + ps.type())
-}
-if(gpio == "" || gpio == undefined || gpio == null) {
-throw new Error("Fan pin not defined")
-}
-if(isNaN(Number(gpio))) {
-throw new Error("Fan pin is not a number " + gpio)
-}
-const fan = new Gpio(gpio,'out');
-
-if(typeof val !== 'object') {
-maxT = "55"
-minT = "45"
-setInterval(function() {
-degrec.get(res => {
-if(res > maxT || res == maxT) {
-	fan.writeSync(1)
-} else if(res < minT ||res == minT) {
-	fan.writeSync(0)
-}
-})
-},100)
-} else {
-if(val.min == "" || val.min == undefined || isNaN(Number(val.min)) || val.max == "" || val.max == undefined || isNaN(Number(val.max))) {
-throw new Error("min,max value not defined in: " + val + " object")	
-}
-setInterval(function() {
-degrec.get(res => {
-if(res > val.max || res == val.max) {
-	fan.writeSync(1)
-} else if(res < val.min ||res == val.min) {
-	fan.writeSync(0)
-}
-})
-},100)
-}
-
-process.on('SIGINT', () => {
-fan.writeSync(0)
-process.exit()
-});
+	if(fanPin instanceof Gpio) {
+		var {maxT, minT} = {min: min && !isNaN(Number(min)) || 45, maxT: max && !isNaN(Number(max)) || 55}
+		setInterval(async () => {
+			var cpuDegree = await degrec.get();
+			res > maxT || res == maxT ? fanPin.writeSync(1) : fanPin.writeSync(0) 
+		}, 100)
+	} else {
+		throw new Error("fanPin is not a Gpio pin")
+	}
 }
